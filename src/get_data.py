a85877bb6fcb9067602c6ad622cd5d7dd424438c
shuklasid19 <shuklasid19@gmail.com>
2022-04-05 05:10:07 -0700

    first_stage

--- src/get_data.py
+++ src/get_data.py
@@ -4,12 +4,13 @@
 #config is params.yaml all information stored in the form of dictionary
 #read params take params and iterate over it and returns
 
-
+## read params
+## process
+## return dataframe
 import os
 import yaml
 import pandas as pd
 import argparse
-
 
 def read_params(config_path):
     with open(config_path) as yaml_file:
@@ -18,11 +19,11 @@ def read_params(config_path):
 
 def get_data(config_path):
     config = read_params(config_path)
+    # print(config)
     data_path = config["data_source"]["s3_source"]
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
-    print(df.head())
     return df
-    
+
 
 
 if __name__=="__main__":
